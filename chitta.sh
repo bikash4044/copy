77af5b3d88405115e8f8443d59bcaaa4e45a536a
Bikash Ranjan Behera <90145679+bikash4044@users.noreply.github.com>
2024-11-19 15:24:13 +0530

    Create chitta.sh

---
+++ chitta.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+# Transfer each directory using sshpass and scp with the password directly included
+sshpass -p 'AP@01JUNE' scp -r ./2011/June/wrfout* wre@10.26.3.215:/home/wre/lab_5tb/chitta/2011/jun/
+sshpass -p 'AP@01JUNE' scp -r ./2011/July/wrfout* wre@10.26.3.215:/home/wre/lab_5tb/chitta/2011/jul/
+sshpass -p 'AP@01JUNE' scp -r ./2011/Aug/wrfout* wre@10.26.3.215:/home/wre/lab_5tb/chitta/2011/aug/
+sshpass -p 'AP@01JUNE' scp -r ./2011/Sep/wrfout* wre@10.26.3.215:/home/wre/lab_5tb/chitta/2011/sep/
+
+sshpass -p 'AP@01JUNE' scp -r ./2012/June/wrfout* wre@10.26.3.215:/home/wre/lab_5tb/chitta/2012/jun/
+sshpass -p 'AP@01JUNE' scp -r ./2012/July/wrfout* wre@10.26.3.215:/home/wre/lab_5tb/chitta/2012/jul/
+sshpass -p 'AP@01JUNE' scp -r ./2012/Aug/wrfout* wre@10.26.3.215:/home/wre/lab_5tb/chitta/2012/aug/
+sshpass -p 'AP@01JUNE' scp -r ./2012/Sep/wrfout* wre@10.26.3.215:/home/wre/lab_5tb/chitta/2012/sep/
+
+sshpass -p 'AP@01JUNE' scp -r ./2013/June/wrfout* wre@10.26.3.215:/home/wre/lab_5tb/chitta/2013/jun/
+sshpass -p 'AP@01JUNE' scp -r ./2013/July/wrfout* wre@10.26.3.215:/home/wre/lab_5tb/chitta/2013/jul/
+sshpass -p 'AP@01JUNE' scp -r ./2013/Aug/wrfout* wre@10.26.3.215:/home/wre/lab_5tb/chitta/2013/aug/
+sshpass -p 'AP@01JUNE' scp -r ./2013/Sep/wrfout* wre@10.26.3.215:/home/wre/lab_5tb/chitta/2013/sep/
+
+sshpass -p 'AP@01JUNE' scp -r ./2014/June/wrfout* wre@10.26.3.215:/home/wre/lab_5tb/chitta/2014/jun/
+sshpass -p 'AP@01JUNE' scp -r ./2014/July/wrfout* wre@10.26.3.215:/home/wre/lab_5tb/chitta/2014/jul/
+sshpass -p 'AP@01JUNE' scp -r ./2014/Aug/wrfout* wre@10.26.3.215:/home/wre/lab_5tb/chitta/2014/aug/
+sshpass -p 'AP@01JUNE' scp -r ./2014/Sep/wrfout* wre@10.26.3.215:/home/wre/lab_5tb/chitta/2014/sep/
+
+sshpass -p 'AP@01JUNE' scp -r ./2015/June/wrfout* wre@10.26.3.215:/home/wre/lab_5tb/chitta/2015/jun/
+sshpass -p 'AP@01JUNE' scp -r ./2015/July/wrfout* wre@10.26.3.215:/home/wre/lab_5tb/chitta/2015/jul/
+sshpass -p 'AP@01JUNE' scp -r ./2015/Aug/wrfout* wre@10.26.3.215:/home/wre/lab_5tb/chitta/2015/aug/
+sshpass -p 'AP@01JUNE' scp -r ./2015/Sep/wrfout* wre@10.26.3.215:/home/wre/lab_5tb/chitta/2015/sep/
